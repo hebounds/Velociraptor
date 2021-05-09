cf6c80ebd356ba3516c4a07c97760d872404f4d4
hayes <ervinbounds@gmail.com>
2021-05-09 00:51:05 -0500

    initial

--- src/ymlHandler.py
+++ src/ymlHandler.py
@@ -1,2 +1,10 @@
-import pyYAML
+import yaml
 
+try:
+    with open('users.yml', 'r+') as file_object:
+        data = file_object.read()
+        print(data)
+except:
+    print('Velociraptor - Error occurred when reading User Data file')
+
+yaml.load(data)
