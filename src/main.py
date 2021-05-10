a68192b104df745e5424ecad788162c8e1d33ef1
XarkenZ <46010001+xarkenz@users.noreply.github.com>
2021-05-09 23:22:30 -0500

    General code cleanup
    Bell command now uses global arrays instead of hardcoded values, made code slightly more readable by reducing single-line statements

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-import discord
-from discord.ext import commands
+import calendar
 import os
 from datetime import date
-import calendar
+
+import discord
+from discord.ext import commands
+
 import ymlHandler
 
 errors = 0
@@ -11,6 +13,35 @@ color_lasa_blue = 0x0c2340
 color_red = 0xe84b33
 color_green = 0x72e02d
 color_yellow = 0xffef42
+
+bells_advisory = [
+    ["1/5",      "08:15", "09:40", "85"],
+    ["Advisory", "09:45", "10:35", "50"],
+    ["2/6",      "10:40", "12:05", "85"],
+    ["Lunch",    "12:05", "12:55", "50"],
+    ["3/7",      "13:00", "14:25", "85"],
+    ["4/8",      "14:30", "15:55", "85"]
+]
+
+bells_non_advisory = [
+    ["1/5",      "08:15", "09:50", "95"],
+    ["2/6",      "09:55", "11:30", "95"],
+    ["Lunch",    "11:35", "12:35", "60"],
+    ["3/7",      "12:40", "14:15", "95"],
+    ["4/8",      "14:20", "15:55", "95"]
+]
+
+bells_c_day = [
+    ["1",        "08:15", "09:00", "45"],
+    ["2",        "09:05", "09:50", "45"],
+    ["3",        "09:55", "10:40", "45"],
+    ["4",        "10:45", "11:30", "45"],
+    ["Lunch",    "11:35", "12:35", "60"],
+    ["5",        "12:40", "13:25", "45"],
+    ["6",        "13:30", "14:15", "45"],
+    ["7",        "14:20", "15:05", "45"],
+    ["8",        "15:10", "15:55", "45"]
+]
 
 bot = commands.Bot(command_prefix="lasa ")
 
@@ -23,7 +54,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if not message.author.bot: # 
+    if not message.author.bot:
         await bot.process_commands(message)
 
 
@@ -40,22 +71,29 @@ async def on_command_error(ctx, error):
 @bot.command(aliases=["notice", "event", "noticeboard", "notice-board", "announce", "announcement"])
 async def board(ctx, *title):
     if ctx.guild.get_role(610536160338378765) in ctx.author.roles:
+        # Invoker has Staff role, proceed
         title = " ".join(title)
         if title == "": title = "Announcement"
-        
-        box = discord.Embed(title="Create an Announcement", description="This announcement will show up in <#615257559976247300>. Use the :vibration_mode: reaction to mention @everyone when the announcement is sent.", color=color_lasa_blue)
+
+        box = discord.Embed(title="Create an Announcement",
+                            description="This announcement will show up in <#615257559976247300>. Use the :vibration_mode: reaction to mention @everyone when the announcement is sent.", color=color_lasa_blue)
         box.add_field(name="Announcement Title", value=title, inline=False)
-        box.add_field(name="Content", value="The next message you send will be set as the description of this announcement. Press :stop_sign: to cancel.", inline=False)
+        box.add_field(
+            name="Content", value="The next message you send will be set as the description of this announcement. Press :stop_sign: to cancel.", inline=False)
         bot_msg = await ctx.send(embed=box)
-        
+
+        # Add reactions for user options
         mention = "📳"
         cancel = "🛑"
         await bot_msg.add_reaction(cancel)
         await bot_msg.add_reaction(mention)
-        
+
+        # Wait until the user sends another message (if cancel reaction is set, message is simply ignored)
         description_msg = await bot.wait_for("message", check=lambda msg: msg.author == ctx.author and msg.channel == ctx.channel)
-        bot_msg = await ctx.channel.fetch_message(bot_msg.id) # There should probably be a discord.Message.fetch_reactions() method but this works too
-        
+        # There should probably be a discord.Message.fetch_reactions() method but refreshing the message again works
+        bot_msg = await ctx.channel.fetch_message(bot_msg.id)
+
+        # Check the message reactions (sort of gross)
         cancel_flag = False
         important_flag = False
         for r in bot_msg.reactions:
@@ -71,12 +109,15 @@ async def board(ctx, *title):
                     continue
         
         if not cancel_flag:
+            # User wants to proceed; send announcement
             box = discord.Embed(title=title, description=description_msg.content, color=color_lasa_blue)
             box.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
             noticeboard = bot.get_channel(615257559976247300)
             if important_flag:
+                # Mention @everyone in addition to sending the announcement
                 await noticeboard.send("@everyone", embed=box)
             else:
+                # Do not mention @everyone
                 await noticeboard.send(embed=box)
             await ctx.send(embed=discord.Embed(title="Announcement posted.", color=color_green))
     else:
@@ -85,8 +126,14 @@ async def board(ctx, *title):
 
 @bot.command(aliases=["bells"])
 async def bell(ctx, version="today", *args):
+
+    def add_block_fields(embed: discord.Embed, blocks: list):
+        for block in blocks:
+            embed.add_field(name=block[0], value=f"`{block[1]} - {block[2]} ({block[3]})`", inline=False)
+
     version += " ".join(args)
-    
+
+    # Account for next/previous day by adding offset during autoselect
     if version.lower() in ["tomorrow", "next"]:
         day_offset = 1
         version = "today"
@@ -98,66 +145,66 @@ async def bell(ctx, version="today", *args):
     
     autoselect_flag = False
     if version.lower() in ["today", "now", "current"]:
+        # Autoselect a bell schedule according a normal week
         autoselect_flag = True
         day = (date.today().weekday() + day_offset) % 7
-        if day in [5, 6]: # Weekend
+        if day in [5, 6]:
+            # Saturday, Sunday
             box = discord.Embed(title="No bell schedule found for this day.", color=color_red)
             await ctx.send(embed=box)
             return
-        elif day in [1, 2]: # Tue/Wed (advisory)
+        elif day in [1, 2]:
+            # Tuesday, Wednesday
             version = "advisory"
-        else: # Non-advisory
+        else:
+            # Monday, Thursday, (Friday)
             version = "non-advisory"
-    
+
     if version.lower() in ["adv", "advisory"]:
-        box = discord.Embed(title=(f"{calendar.day_name[day]} Bell Schedule - Advisory" if autoselect_flag else "Bell Schedule - Advisory"), color=color_lasa_blue)
-        box.add_field(name="Period 1/5", value="8:15 am - 9:40 am (85)", inline=False)
-        box.add_field(name="Advisory", value="9:45 am - 10:35 am (50)", inline=False)
-        box.add_field(name="Period 2/6", value="10:40 am - 12:05 pm (85)", inline=False)
-        box.add_field(name="Lunch", value="12:05 pm - 12:55 pm (50)", inline=False)
-        box.add_field(name="Period 3/7", value="1:00 pm - 2:25 pm (85)", inline=False)
-        box.add_field(name="Period 4/8", value="2:30 pm - 3:55 pm (85)", inline=False)
-        if autoselect_flag: box.set_footer(text="Make sure to verify that this is the schedule being used!") # Schedule is autoselected by weekday, exceptions not taken into account
-    
+        # Advisory schedule (can be autoselected)
+        if autoselect_flag:
+            box = discord.Embed(title=f"{calendar.day_name[day]} Bell Schedule - Advisory", color=color_lasa_blue)
+        else:
+            box = discord.Embed(title="Bell Schedule - Advisory", color=color_lasa_blue)
+        add_block_fields(box, bells_advisory)
+        if autoselect_flag:
+            # Some weeks are exceptional to the regular schedule
+            box.set_footer(text="Make sure to verify that this is the schedule being used!")
+
     elif version.lower() in ["reg", "norm", "regular", "normal", "nonadv", "non-adv", "nonadvisory", "non-advisory"]:
-        box = discord.Embed(title=(f"{calendar.day_name[day]} Bell Schedule - Non-Advisory" if autoselect_flag else "Bell Schedule - Non-Advisory"), color=color_lasa_blue)
-        box.add_field(name="Period 1/5", value="8:15 am - 9:50 am (95)", inline=False)
-        box.add_field(name="Period 2/6", value="9:55 am - 11:30 am (95)", inline=False)
-        box.add_field(name="Lunch", value="11:35 am - 12:35 pm (60)", inline=False)
-        box.add_field(name="Period 3/7", value="12:40 pm - 2:15 pm (95)", inline=False)
-        box.add_field(name="Period 4/8", value="2:20 pm - 3:55 pm (95)", inline=False)
-        if autoselect_flag: box.set_footer(text="Make sure to verify that this is the schedule being used!") # Same reason as above
-    
+        # Regular schedule (can be autoselected)
+        if autoselect_flag:
+            box = discord.Embed(title=f"{calendar.day_name[day]} Bell Schedule - Non-Advisory", color=color_lasa_blue)
+        else:
+            box = discord.Embed(title="Bell Schedule - Non-Advisory", color=color_lasa_blue)
+        add_block_fields(box, bells_non_advisory)
+        if autoselect_flag:
+            # Some weeks are exceptional to the regular schedule
+            box.set_footer(text="Make sure to verify that this is the schedule being used!")
+
     elif version.lower() in ["c", "cday", "c-day"]:
-        box = discord.Embed(title="Bell Schedule - C Day", color=color_lasa_blue)
-        box.add_field(name="Period 1", value="8:15 am - 9:00 am (45)", inline=False)
-        box.add_field(name="Period 2", value="9:05 am - 9:50 am (45)", inline=False)
-        box.add_field(name="Period 3", value="9:55 am - 10:40 am (45)", inline=False)
-        box.add_field(name="Period 4", value="10:45 am - 11:30 am (45)", inline=False)
-        box.add_field(name="Lunch", value="11:35 am - 12:35 pm (60)", inline=False)
-        box.add_field(name="Period 5", value="12:40 pm - 1:25 pm (45)", inline=False)
-        box.add_field(name="Period 6", value="1:30 pm - 2:15 pm (45)", inline=False)
-        box.add_field(name="Period 7", value="2:20 pm - 3:05 pm (45)", inline=False)
-        box.add_field(name="Period 8", value="3:10 pm - 3:55 pm (45)", inline=False)
-        # C day schedule will never be autoselected
-    
+        # C day schedule (all 8 periods, never autoselected as it rarely happens)
+        box=discord.Embed(title="Bell Schedule - C Day", color=color_lasa_blue)
+        add_block_fields(box, bells_c_day)
+
     else:
-        box = discord.Embed(title="Unknown bell schedule.", color=color_red)
-    
+        box=discord.Embed(title="Unknown bell schedule.", color=color_red)
+
     await ctx.send(embed=box)
 
 
 @bot.command(aliases=["schedules"])
 async def schedule(ctx, cmd, *args):
     if cmd.lower() in ["optout", "opt-out"]:
-        pass # Opt out of the schedule function
+        pass  # TODO: Opt out of the schedule function
     elif cmd.lower() in ["optin", "opt-in", "opt"]:
-        pass # Opt in the schedule function
+        pass  # TODO: Opt in the schedule function
     else:
-        box = discord.Embed(title="Unknown schedule function.", color=color_red)
-        await ctx.send(embed=box) # Shift down a tab once above functions filled in
+        box=discord.Embed(title="Unknown schedule function.", color=color_red)
+        # TODO: Shift down a tab once above functions filled in
+        await ctx.send(embed=box)
 
 
 # We probably don't want to run the bot if it's being imported in another program
 if __name__ == "__main__":
-    bot.run(os.getenv("TOKEN")) # details in the README
+    bot.run(os.getenv("TOKEN"))  # TODO: explain in readme
