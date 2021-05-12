8e0ccdb8a43ba2ad56926f82528aa631d0a02105
hayes <ervinbounds@gmail.com>
2021-05-12 00:13:43 -0500

    fixed open

--- src/ymlHandler.py
+++ src/ymlHandler.py
@@ -1,9 +1,10 @@
 import yaml
+import os
 
 
 def open_yamlfile(fileStr):
     try:
-        with open(fileStr, 'w+') as file_object:
+        with open("src/" + fileStr, 'r+') as file_object:
             data = file_object
             data = yaml.load(data, Loader=yaml.FullLoader)
             print(data)
@@ -11,14 +12,18 @@ def open_yamlfile(fileStr):
     except:
         raise Exception('Velociraptor - Error occurred when reading YAML data file')
 
+
 def add_field(dataFile, key, value):
     pass
 
+
 def get_field(dataFile, key):
     for item, doc in dataFile.items():
-        print(item, ":", doc)
+        if item == key:
+            return item
     pass
+
 
 userData = open_yamlfile('users.yml')
 
-get_field(userData, 'myface')
+print(get_field(userData, 'myface'))
